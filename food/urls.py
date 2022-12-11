da84239a8b8fa31f57fa303fb6402b1ab2a45f0b
GianlorenzoDercole <johndercole01@gmail.com>
2022-12-11 14:35:37 -0800

    added links to item info see detail.html and index.html

--- food/urls.py
+++ food/urls.py
@@ -3,6 +3,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    #/food/
     path('',views.index,name='index'),
-    path('item/',views.item,name='item')
+
+    path('item/',views.item,name='item'),
+    #/food/1(item id)
+    path('<int:item_id>/',views.detail,name='detail')
 ]
