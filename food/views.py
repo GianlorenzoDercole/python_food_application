1fb0d9c259bc5d444869503379f8678f394c82ea
GianlorenzoDercole <johndercole01@gmail.com>
2023-01-19 14:23:29 -0800

    changed index view to a class based view see views.py

--- food/views.py
+++ food/views.py
@@ -4,16 +4,27 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
 # Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-    # template = loader.get_template('food/index.html')
-    context ={
-        'item_list':item_list,
-    }
-    # return HttpResponse(template.render(context,request))
-    return render(request,'food/index.html',context)
+
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+
+
+# def index(request):
+#     item_list = Item.objects.all()
+#     # template = loader.get_template('food/index.html')
+#     context ={
+#         'item_list':item_list,
+#     }
+#     # return HttpResponse(template.render(context,request))
+#     return render(request,'food/index.html',context)
+
 def item(request):
     return HttpResponse('<h1>this is an item view</h1>')
 
